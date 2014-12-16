2dd0eb9b3170f09023d4a714bc4584dad3a2c9fd
Xing, Wang <wang.xing.me@gmail.com>
2014-12-16 21:18:05 +0800

    change route & clean default play controller/view/routes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,12 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.twirl.api.Html
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(title="Welcome, zenja!", message="hello world!"))
   }
 
 }
