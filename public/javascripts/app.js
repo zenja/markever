43c3345ec7c120f67aa1090fe518c4963e362de6
Xing, Wang <wang.xing.me@gmail.com>
2014-12-16 21:16:37 +0800

    add enml-js files

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,47 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
-}
+$(document).ready(function() {
+    var opts = {
+      container: 'epiceditor',
+      textarea: null,
+      basePath: 'epiceditor',
+      clientSideStorage: true,
+      localStorageName: 'epiceditor',
+      useNativeFullscreen: false,
+      parser: marked,
+      file: {
+        name: 'epiceditor',
+        defaultContent: '',
+        autoSave: 100
+      },
+      theme: {
+        base: '/themes/base/epiceditor.css',
+        preview: '/themes/preview/preview-dark.css',
+        editor: '/themes/editor/epic-dark.css'
+      },
+      button: {
+        preview: true,
+        fullscreen: true,
+        bar: "auto"
+      },
+      focusOnLoad: false,
+      shortcut: {
+        modifier: 18,
+        fullscreen: 70,
+        preview: 80
+      },
+      string: {
+        togglePreview: 'Toggle Preview Mode',
+        toggleEdit: 'Toggle Edit Mode',
+        toggleFullscreen: 'Enter Fullscreen'
+      },
+      autogrow: false
+    }
+    var editor = new EpicEditor().load();
+
+    $("a#show_html").click(function() {
+        alert(editor.getElement('editor').body.innerHTML);
+    });
+
+    $("a#preview").click(function() {
+        editor.preview();
+    });
+});
