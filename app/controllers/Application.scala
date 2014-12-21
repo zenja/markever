745d889ea9a468f12b94749a521158a635b8af4f
Xing, Wang <wang.xing.me@gmail.com>
2014-12-21 21:56:41 +0800

    better redirect for /clear & refactor Application controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,8 +6,13 @@ import play.twirl.api.Html
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index(title="Welcome, zenja!"))
+  def index = Action { implicit request =>
+    // if has auth info, show main page
+    if (request.session.get("token").isDefined) {
+      Ok(views.html.index())
+    } else {
+      Redirect(routes.Application.start_oauth())
+    }
   }
 
   def start_oauth = Action { implicit request =>
@@ -22,7 +27,7 @@ object Application extends Controller {
    * Clear session
    */
   def clear = Action { implicit request =>
-    Ok(views.html.index(title="Session cleared!")).withNewSession
+    Redirect(routes.Application.index).withNewSession
   }
 
 }
