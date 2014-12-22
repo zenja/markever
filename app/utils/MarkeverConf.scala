26f8f0aca199bd0dbec21389c9e0a52716b67ad5
Xing, Wang <wang.xing.me@gmail.com>
2014-12-22 22:29:22 +0800

    add Markever conf

--- app/utils/MarkeverConf.scala
+++ app/utils/MarkeverConf.scala
@@ -4,12 +4,11 @@ import java.io.FileInputStream
 import java.util.Properties
 
 object MarkeverConf {
-  val (application_tag_name, application_tag_value, application_version) = {
+  val (application_identifier, application_version) = {
     val prop = new Properties()
     prop.load(new FileInputStream("conf/markever.properties"))
     (
-      prop.getProperty("markever.application.tag.name"),
-      prop.getProperty("markever.application.tag.value"),
+      prop.getProperty("markever.application.identifier"),
       prop.getProperty("markever.application.version")
       )
   }
